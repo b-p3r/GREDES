b207cd2ee47c2ea255b1c0e861174b79fddc8bc0
_bpp <pereirabruno05@gmail.com>
2017-01-13 02:15:04 +0000

    Adiciona classe Chart para criação de gráficos

---
+++ src/di/uminho/miei/gredes/presentationlayer/components/chart/Chart.java
@@ -0,0 +1,67 @@
+package di.uminho.miei.gredes.presentationlayer.components.chart;
+
+import java.awt.Color;
+import java.util.TreeSet;
+
+import info.monitorenter.gui.chart.Chart2D;
+import info.monitorenter.gui.chart.ITrace2D;
+import info.monitorenter.gui.chart.TracePoint2D;
+import info.monitorenter.gui.chart.traces.Trace2DSorted;
+
+public class Chart extends Chart2D {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -8624502985053219743L;
+
+	private int numPointsShowing;
+
+	private ITrace2D trace;
+	private TreeSet<TracePoint2D> pointTracker;
+
+	public Chart(int numPointsShowing) {
+		super();
+		this.numPointsShowing = numPointsShowing;
+		this.trace = new Trace2DSorted();
+		this.pointTracker = new TreeSet<>(new TracePoint2DComparator());
+		trace.setColor(Color.RED);
+		this.addTrace(trace);
+		
+
+	}
+
+	public  int getNumPointsShowing() {
+		return this.numPointsShowing;
+	}
+
+	public void setNumPointsShowing(int numPointsShowing) {
+		this.numPointsShowing = numPointsShowing;
+	}
+
+	public  ITrace2D getTrace() {
+		return trace;
+	}
+
+	public TreeSet<TracePoint2D> getPointTracker() {
+		return pointTracker;
+	}
+
+	public void addPoint(double x, double y) {
+
+		
+		pointTracker.add(new TracePoint2D(x, y));
+		trace.addPoint(new TracePoint2D(x, y));
+		System.out.println("X : " + pointTracker.first().getX() + " Y :"+pointTracker.first().getY());
+
+		if (trace.getSize() == this.numPointsShowing) {
+
+			TracePoint2D point = pointTracker.pollFirst();
+			trace.removePoint((TracePoint2D) point);
+
+		}
+	}
+	
+	
+
+}
